5c916081c1e991f66183fc1d0488d729e580ad01
jonathanludena <jonathanludenai@gmail.com>
2022-08-29 18:42:34 -0500

    [add] model user

---
+++ models/user.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/* User model to DB MongoDB */
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name     string             `bson:"name" json:"name,omitempty"`
+	Lastname string             `bson:"lastname" json:"lastname,omitempty"`
+	Birthday time.Time          `bson:"birthday" json:"birthday,omitempty"`
+	Email    string             `bson:"email" json:"email"`
+	Password string             `bson:"password" json:"password,omitempty"`
+	Avatar   string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner   string             `bson:"banner" json:"banner,omitempty"`
+	Bio      string             `bson:"bio" json:"bio,omitempty"`
+	Location string             `bson:"location" json:"location,omitempty"`
+	Siteweb  string             `bson:"siteweb" json:"siteWeb,omitempty"`
+}
